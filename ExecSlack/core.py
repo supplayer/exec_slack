384f46c56338f184985d36949764edc13ff44a77
supplayer <x254724521@hotmail.com>
2022-03-23 00:20:16 +0800

    optimize ExecSlack.core.Conversations

--- ExecSlack/core.py
+++ ExecSlack/core.py
@@ -22,22 +22,23 @@ class Conversations:
     def __init__(self, app_session: Web):
         self.__session = app_session
 
-    def list(self, exclude_archived=False, limit=100, team_id=None, types="public_channel"):
+    def list_iter(self, exclude_archived=False, limit=100, team_id=None, types="public_channel") -> iter:
         url, channel_list = 'https://slack.com/api/conversations.list', []
         params = dict(cursor=None, exclude_archived=exclude_archived, limit=limit, team_id=team_id, types=types)
-        self.__cursor_list(url, params, channel_list, 'channels')
-        return channel_list
+        for channel in self.__cursor_list(url, params, channel_list, 'channels'):
+            yield channel
 
-    def history(self, channel_id, latest=None, oldest=0, limit=100, inclusive=True):
+    def history_iter(self, channel_id, latest=None, oldest=0, limit=100, inclusive=True) -> iter:
         url, history_list = 'https://slack.com/api/conversations.history', []
         params = dict(channel=channel_id, cursor=None, latest=latest, oldest=oldest, limit=limit, inclusive=inclusive)
-        self.__cursor_list(url, params, history_list, 'messages')
-        return history_list
+        for history in self.__cursor_list(url, params, history_list, 'messages'):
+            yield history
 
     def __cursor_list(self, url: str, params: dict, record_list: list, data_field: str):
         while params['cursor'] or params['cursor'] is None:
             data = self.__session.post(url, params=params).json()
-            record_list += [history for history in data[data_field]]
+            for item in data[data_field]:
+                yield item
             params['cursor'] = data['response_metadata'].get("next_cursor", '')
 
 
@@ -54,7 +55,8 @@ class Chat:
     def clear(self, channel_id, **del_time_offset):
         del_time_offset = del_time_offset or {"days": 90}
         latest = datetime.now().timestamp() - timedelta(**del_time_offset).total_seconds()
-        return [self.delete(channel_id, i['ts']) for i in self.__conversations.history(channel_id, latest=latest)]
+        for i in self.__conversations.history_iter(channel_id, latest=latest):
+            print(self.delete(channel_id, i['ts']))
 
 
 class Slack:
